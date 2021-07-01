136f81e073510ee941aa9be8a8a94d000fd52dc0
Victor Ariel Leal Sobral <31193893+victorsobral@users.noreply.github.com>
2021-07-01 17:04:10 -0400

    Update TEG_profiler_upload.py
    bug fixes

--- TEG_profiler_upload.py
+++ TEG_profiler_upload.py
@@ -7,7 +7,7 @@ import json
 storage_path = '/home/pi/Desktop/shared/data'
 
 with open("/home/pi/Desktop/Application_info.txt") as json_appInfo:
-    APP_INFO = json.load(json_appInfo)
+	APP_INFO = json.load(json_appInfo)
 
 APP_ID = APP_INFO["APP_ID"]
 
@@ -15,7 +15,7 @@ filename_list = [f for f in listdir(storage_path) if isfile(join(storage_path, f
 
 
 for filename in filename_list:
-	with open(filename) as csv_file:
+	with open(join(storage_path, filename)) as csv_file:
 		r=requests.post("http://73.251.37.2:1237/upload", files={'upload':csv_file}, headers={'APP_ID':APP_ID})
 
 
