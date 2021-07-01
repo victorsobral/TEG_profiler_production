62f8fe28296698138c3f3b0ce4d1aebd01f8e7d9
Victor Ariel Leal Sobral <31193893+victorsobral@users.noreply.github.com>
2021-07-01 17:00:43 -0400

    Create TEG_profiler_upload.py
    Upload script

---
+++ TEG_profiler_upload.py
@@ -0,0 +1,23 @@
+from os import listdir
+from os.path import isfile, join
+
+import requests
+import json
+
+storage_path = '/home/pi/Desktop/shared/data'
+
+with open("/home/pi/Desktop/Application_info.txt") as json_appInfo:
+    APP_INFO = json.load(json_appInfo)
+
+APP_ID = APP_INFO["APP_ID"]
+
+filename_list = [f for f in listdir(storage_path) if isfile(join(storage_path, f))]
+
+
+for filename in filename_list:
+	with open(filename) as csv_file:
+		r=requests.post("http://73.251.37.2:1237/upload", files={'upload':csv_file}, headers={'APP_ID':APP_ID})
+
+
+
+
