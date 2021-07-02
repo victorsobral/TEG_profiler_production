c4b6b3172f29e1e2a7dd1b89520038d1c6bdf7c0
Victor Ariel Leal Sobral <31193893+victorsobral@users.noreply.github.com>
2021-07-02 10:58:49 -0400

    Update TEG_profiler_upload.py
    bug fixes

--- TEG_profiler_upload.py
+++ TEG_profiler_upload.py
@@ -17,7 +17,7 @@ filename_list = [f for f in listdir(storage_path) if isfile(join(storage_path, f
 with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt', 'w') as file:
 	csvwriter = csv.writer(file)
 	for row in filename_list:
-		csvwriter.writerow(row)
+		csvwriter.writerow([row])
 
 with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt') as txt_file:
 	r=requests.post("http://73.251.37.2:1237/upload", files={'upload':txt_file}, headers={'APP_ID':APP_ID})
