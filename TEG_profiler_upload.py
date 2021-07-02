8fd0c7e2597822d06b77cc54aeb46bce72d15f19
Victor Ariel Leal Sobral <31193893+victorsobral@users.noreply.github.com>
2021-07-02 10:51:08 -0400

    Update TEG_profiler_upload.py
    updated to use csv writer

--- TEG_profiler_upload.py
+++ TEG_profiler_upload.py
@@ -1,11 +1,9 @@
 from os import listdir
 from os.path import isfile, join
-
 import requests
-
 import json
-
 import time
+import csv
 
 storage_path = '/home/pi/Desktop/shared/data'
 
@@ -17,7 +15,9 @@ APP_ID = APP_INFO["APP_ID"]
 filename_list = [f for f in listdir(storage_path) if isfile(join(storage_path, f))]
 
 with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt', 'w') as file:
-    file.writelines(filename_list)
+	csvwriter = csv.writer(file)
+	for row in filename_list:
+		csvwriter.writerow(row)
 
 with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt') as txt_file:
 	r=requests.post("http://73.251.37.2:1237/upload", files={'upload':txt_file}, headers={'APP_ID':APP_ID})
