59c126b7d1b18225dfd3b6c0b5de0c416c6a0745
Victor Ariel Leal Sobral <31193893+victorsobral@users.noreply.github.com>
2021-07-02 11:16:32 -0400

    Update TEG_profiler_cloud.py
    update

--- TEG_profiler_cloud.py
+++ TEG_profiler_cloud.py
@@ -1,11 +1,9 @@
 from os import listdir
 from os.path import isfile, join
-
 import requests
-
 import json
-
-# import time
+import time
+import csv
 
 storage_path = '/home/pi/Desktop/shared/data'
 
@@ -16,11 +14,19 @@ APP_ID = APP_INFO["APP_ID"]
 
 filename_list = [f for f in listdir(storage_path) if isfile(join(storage_path, f))]
 
+with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt', 'w') as file:
+	csvwriter = csv.writer(file)
+	for row in filename_list:
+		csvwriter.writerow([row])
+
+with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt') as txt_file:
+	r=requests.post("http://73.251.37.2:1237/upload", files={'upload':txt_file}, headers={'APP_ID':APP_ID})
+	time.sleep(0.2)
 
 for filename in filename_list:
 	with open(join(storage_path, filename)) as csv_file:
 		r=requests.post("http://73.251.37.2:1237/upload", files={'upload':csv_file}, headers={'APP_ID':APP_ID})
-		# time.sleep(2)
+		time.sleep(0.2)
 
 
 
