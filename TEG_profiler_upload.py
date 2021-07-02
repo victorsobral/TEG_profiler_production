358ee2f78bc9dc541c01e7b1049a39bb63b1f646
Victor Ariel Leal Sobral <31193893+victorsobral@users.noreply.github.com>
2021-07-02 10:41:03 -0400

    Update TEG_profiler_upload.py
    added TEG_local_storage_list.txt file to check list of local files

--- TEG_profiler_upload.py
+++ TEG_profiler_upload.py
@@ -5,7 +5,7 @@ import requests
 
 import json
 
-# import time
+import time
 
 storage_path = '/home/pi/Desktop/shared/data'
 
@@ -16,11 +16,17 @@ APP_ID = APP_INFO["APP_ID"]
 
 filename_list = [f for f in listdir(storage_path) if isfile(join(storage_path, f))]
 
+with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt', 'w') as file:
+    file.writelines(filename_list)
+
+with open('/home/pi/Desktop/shared/TEG_local_storage_list.txt') as txt_file:
+	r=requests.post("http://73.251.37.2:1237/upload", files={'upload':txt_file}, headers={'APP_ID':APP_ID})
+	time.sleep(0.5)
 
 for filename in filename_list:
 	with open(join(storage_path, filename)) as csv_file:
 		r=requests.post("http://73.251.37.2:1237/upload", files={'upload':csv_file}, headers={'APP_ID':APP_ID})
-		# time.sleep(2)
+		time.sleep(0.5)
 
 
 
